0a7af9bf08d8c14ea52a51fac1889f90b3089073
rea1stuff <40441464+rea1stuff@users.noreply.github.com>
2022-08-17 15:22:43 +0300

    domain: add spec

---
+++ src/Domain/Specs/UserWithMoviesSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using MovieCatalog.Domain.Entities;
+
+namespace MovieCatalog.Domain.Specs;
+
+public sealed class UserWithMoviesSpec : Specification<User>, ISingleResultSpecification
+{
+    public UserWithMoviesSpec(string uId)
+    {
+        Query.Where(u => u.Id == uId).Include(u => u.Movies);
+    }
+}
